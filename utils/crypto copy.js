0f69208c00f90c588b9f9398309e449cfd88a0e1
VK <007ikun@gmail.com>
2024-12-16 01:12:45 +0800

    1

--- utils/crypto copy.js
+++ utils/crypto copy.js
@@ -1,8 +1,12 @@
 import env from "../env";
+import { isEmpty } from "./index";
 import crypto from "crypto";
 function formatRequestBody(data) {
+	data = convertToNumericRecursive(data)
+	data = sortObjectKeys(data);
     return JSON.stringify(data).replace(/\s+/g, "");
 }
+
 function generateXSk(data) {
 	data += "vk666!*@#?.#@"
     return crypto
@@ -15,5 +19,44 @@ function getSignature(dataDict) {
     const formattedData = formatRequestBody(dataDict);
     return generateXSk(formattedData);
 }
+
+function convertToNumericRecursive(obj) {
+    if(isEmpty(obj)) return ""
+    if (Array.isArray(obj)) {
+        return obj.map(value => convertToNumericRecursive(value));
+    } else if (typeof obj === 'object' && obj !== null) {
+        const newObj = {};
+        for (const key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                newObj[key] = convertToNumericRecursive(obj[key]);
+            }
+        }
+        return newObj;
+    } else if (!isNaN(obj) && obj !== '') {
+        // 转换为数字
+        return obj.toString().includes('.') ? parseFloat(obj) : parseInt(obj, 10);
+    } else {
+        return obj;
+    }
+}
+
+/**
+ * 对对象的键进行排序
+ */
+function sortObjectKeys(obj) {
+    if (typeof obj !== "object" || obj === null) return obj;
+
+    if (Array.isArray(obj)) {
+        return obj.map((item) => sortObjectKeys(item));
+    }
+
+    const sortedKeys = Object.keys(obj).sort();
+    const newObj = {};
+    for (const key of sortedKeys) {
+        newObj[key] = sortObjectKeys(obj[key]);
+    }
+    return newObj;
+}
+
 
 export default getSignature;
